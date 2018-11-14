38cdea4c4564bc609a0e7a7f8b876ac694f71e1b
Farruh Habibullaev <farruh.habibullaev@gmail.comS>
2018-11-14 09:44:49 -0800

    Some companies added!

--- README.md
+++ README.md
@@ -12,3 +12,19 @@ I have created this repo to share my solutions to algorithm & data structure pro
 7. [Goldman Sachs](https://github.com/farruhha/problemo/tree/master/src/leetcode/goldman_sachs)
 8. [Bloomberg](https://github.com/farruhha/problemo/tree/master/src/leetcode/bloomberg)
 9. [Alibaba](https://github.com/farruhha/problemo/tree/master/src/leetcode/alibaba)
+10. [Facebook](https://github.com/farruhha/problemo/tree/master/src/leetcode/facebook)
+11. [Oracle](https://github.com/farruhha/problemo/tree/master/src/leetcode/oracle)
+12. [Uber](https://github.com/farruhha/problemo/tree/master/src/leetcode/uber)
+13. [Adobe](https://github.com/farruhha/problemo/tree/master/src/leetcode/adobe)
+14. [Apple](https://github.com/farruhha/problemo/tree/master/src/leetcode/apple)
+15. [VMWare](https://github.com/farruhha/problemo/tree/master/src/leetcode/vmware)
+16. [IXL](https://github.com/farruhha/problemo/tree/master/src/leetcode/ixl)
+17. [Tencent](https://github.com/farruhha/problemo/tree/master/src/leetcode/tencet)
+18. [Lyft](https://github.com/farruhha/problemo/tree/master/src/leetcode/lyft)
+
+
+
+
+
+
+
---
+++ src/leetcode/microsoft/MergeKSortedList.java
@@ -0,0 +1,70 @@
+package leetcode.microsoft;
+
+public class MergeKSortedList {
+
+/**
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ *
+ * Example:
+ *
+ * Input:
+ * [
+ *   1->4->5,
+ *   1->3->4,
+ *   2->6
+ * ]
+ * Output: 1->1->2->3->4->4->5->6*/
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+        ListNode result = lists[0];
+
+        for (int i = 1; i < lists.length; i++) {
+            result = merge(result, lists[i]);
+        }
+        return result;
+    }
+
+    ListNode merge(ListNode result, ListNode partial) {
+        ListNode response = new ListNode(0);
+        ListNode head = response;
+        while (result != null && partial != null) {
+            if (result.val <= partial.val) {
+                ListNode node = new ListNode(result.val);
+                head.next = node;
+                head = node;
+                result = result.next;
+            } else {
+                ListNode node = new ListNode(partial.val);
+                head.next = node;
+                head = node;
+                partial = partial.next;
+            }
+        }
+        while (result != null) {
+            ListNode node = new ListNode(result.val);
+            head.next = node;
+            head = node;
+            result = result.next;
+        }
+        while (partial != null) {
+            ListNode node = new ListNode(partial.val);
+            head.next = node;
+            head = node;
+            partial = partial.next;
+        }
+
+        return response.next;
+    }
+}
